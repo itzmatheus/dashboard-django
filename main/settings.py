c5e22058d00276c262f40f38d6753666858eb114
Matheus Leite <matheusjoselfm@gmail.com>
2020-06-02 00:32:47 -0300

    ajustando template base de accounts, configurando menu e estrutra do projeto para nova tela de home

--- main/settings.py
+++ main/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'apps.core',
-    'apps.user'
+    'apps.user',
+    'apps.home'
 ]
 
 MIDDLEWARE = [
--- main/urls.py
+++ main/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accounts/', include('apps.user.urls'))
+    path('accounts/', include('apps.user.urls')),
+    path('', include('apps.home.urls'))
 ]
